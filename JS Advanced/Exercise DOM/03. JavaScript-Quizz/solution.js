33060b20a2c4026f787502949624703423213c9f
vallecbg <valioplayofficial@gmail.com>
2019-10-06 16:42:45 +0300

    Added solution

--- JS Advanced/Exercise DOM/03. JavaScript-Quizz/solution.js
+++ JS Advanced/Exercise DOM/03. JavaScript-Quizz/solution.js
@@ -1,42 +1,30 @@
 function solve() {
+  let correctAnswers = ["onclick", "JSON.stringify()", "A programming API for HTML and XML documents"];
+  let rightAnswered = 0;
+  let index = 0;
 
-  let correctAnswer = 0;
-  let tempIndex = 0;
+  Array
+    .from(document.querySelectorAll(".answer-text"))
+    .map((x) => x.addEventListener("click", function nextSection(e) {
 
-
-  Array.from(document
-    .querySelectorAll('.quiz-answer'))
-    .map(x => x.addEventListener('click', function nextQuestion(e) {
-      if (e.target.innerText === 'onclick' ||
-        e.target.innerText === 'JSON.stringify()' ||
-        e.target.innerText === 'A programming API for HTML and XML documents') {
-        correctAnswer++;
+      if (correctAnswers.includes(e.target.innerHTML)) {
+        rightAnswered++;
       }
-      let currentSection = document.querySelectorAll('section')[tempIndex];
-      currentSection.style.display = 'none'
-      currentSection.classList.add('hidden');
 
-      let nextSection = document.querySelectorAll('section')[tempIndex + 1];
+      let currSection = document.querySelectorAll("section")[index];
+      currSection.style.display = "none";
 
-      if (nextSection) {
-        nextSection.classList.remove('hidden');
+      if (document.querySelectorAll("section")[index + 1] !== undefined) {
+        let nextSection = document.querySelectorAll("section")[index + 1];
+        nextSection.style.display = "block";
+        index++;
       } else {
-        showResult(correctAnswer);
+        document.querySelector("#results").style.display = "block";
+        if (rightAnswered !== 3) {
+          document.querySelector("#results h1").innerHTML = `You have ${rightAnswered} right answers`;
+        } else {
+          document.querySelector("#results h1").innerHTML = "You are recognized as top JavaScript fan!";
+        }
       }
-
-      nextSection.style.display = 'block';
-
-      tempIndex++;
     }));
-
-
-  function showResult(correctAnswer) {
-
-    document.querySelector('#results').style.display = 'block';
-    if (correctAnswer === 3) {
-      document.querySelector('#results > li > h1').innerText = 'You are recognized as top JavaScript fan!'
-    } else {
-      document.querySelector('#results > li > h1').innerText = `You have ${correctAnswer} right answers`;
-    }
-  }
 }
--- JS Advanced/Exercise DOM/04. Numpad-Calculator/solution.js
+++ JS Advanced/Exercise DOM/04. Numpad-Calculator/solution.js
@@ -1,4 +1,72 @@
 function solve() {
+    let numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    let operands = ['/', '*', '-', '+'];
+    let point = '.';
+    let equals = '=';
+    const regex = /([0-9\.]+) ([\*\/\-\+]) ([0-9\.]+)/gm;
 
-    //TODO...
+    let output = document.getElementById('expressionOutput');
+    let res = document.getElementById('resultOutput');
+
+    let result;
+    
+    document.querySelector("#calculator > div.top > button").addEventListener("click", function clear(e){
+        output.innerHTML = '';
+        res.innerHTML = '';
+    })
+
+    Array
+    .from(document.querySelectorAll("#calculator > div.keys > button"))
+    .map(x => x.addEventListener("click", function add(e){
+        let val = e.target.value
+        if(val === equals){
+            result = calculate();
+            showResult(result);
+        } else {
+            if(operands.includes(val)){
+                output.innerHTML += ` ${val} `;
+            } else {
+                output.innerHTML += val;
+            }
+        
+        }
+    }));
+
+    function showResult(result){
+        res.innerHTML = result;
+    }
+
+    function calculate(){
+        let m;
+        let leftNum;
+        let operand;
+        let rightNum;
+        while ((m = regex.exec(output.innerHTML)) !== null) {
+            // This is necessary to avoid infinite loops with zero-width matches
+            if (m.index === regex.lastIndex) {
+                regex.lastIndex++;
+            }
+            // The result can be accessed through the `m`-variable.
+            m.forEach((match, groupIndex) => {
+                switch(groupIndex){
+                    case 1: {leftnum = Number(match)} break;
+                    case 2: {operand = match} break;
+                    case 3: {rightNum = Number(match)} break;
+                }
+            });
+        }
+
+        let sum;
+        switch(operand){
+            case '+': {sum = leftnum + rightNum} break;
+            case '-': {sum = leftnum - rightNum} break;
+            case '/': {sum = leftnum / rightNum} break;
+            case '*': {sum = leftnum * rightNum} break;
+        }
+        //CHeck if needed
+        if(sum === undefined || sum === Infinity){
+            sum = NaN;
+        }
+        return sum;
+    }
 }
