a67e0559860cc32198f825fe8253439c43ddbfb0
vallecbg <valioplayofficial@gmail.com>
2019-10-06 18:31:30 +0300

    Added solution

--- JS Advanced/Exercise DOM/05. Order-the-Names/solution.js
+++ JS Advanced/Exercise DOM/05. Order-the-Names/solution.js
@@ -1,4 +1,73 @@
 function solve() {
+    let letters = {
+        A:[], 
+        B:[], 
+        C:[], 
+        D:[], 
+        E:[], 
+        F:[], 
+        G:[], 
+        H:[], 
+        I:[], 
+        J:[], 
+        K:[], 
+        L:[], 
+        M:[], 
+        N:['Nixon'], 
+        O:[], 
+        P:['Peterson'], 
+        Q:[], 
+        R:[], 
+        S:[], 
+        T:[], 
+        U:[], 
+        V:[], 
+        W:[], 
+        X:[], 
+        Y:[], 
+        Z:[]
+    };
 
-    //TODO...
+    const dbIndex = {
+        A: 1,
+        B: 2,
+        C: 3,
+        D: 4,
+        E: 5,
+        F: 6,
+        G: 7,
+        H: 8,
+        I: 9,
+        J: 10,
+        K: 11,
+        L: 12,
+        M: 13,
+        N: 14,
+        O: 15,
+        P: 16,
+        Q: 17,
+        R: 18,
+        S: 19,
+        T: 20,
+        U: 21,
+        V: 22,
+        W: 23,
+        X: 24,
+        Y: 25,
+        Z: 26
+    };
+
+    document.querySelector("#exercise > article > button").addEventListener("click", add);
+
+    function add(){
+        let name = document.querySelector("#exercise > article > input[type=text]").value;
+        let firstLetter = name[0].toUpperCase();
+        
+        let asd = firstLetter + name.substring(1).toLowerCase();
+        letters[firstLetter].push(asd);
+
+        let listItems = Array.from(document.querySelectorAll('ol li'));
+
+        listItems[dbIndex[firstLetter] - 1].textContent = letters[firstLetter].join(', ');
+    }
 }
--- JS Advanced/Exercise DOM/06. Table-Search-Engine/solution.js
+++ JS Advanced/Exercise DOM/06. Table-Search-Engine/solution.js
@@ -1,3 +1,51 @@
+// function solve() {
+//    document.querySelector("#searchBtn").addEventListener("click", search);
+
+//    function search() {
+//       let input = document.querySelector("#searchField").value;
+//       if (input !== '') {
+//          let counter = 0;
+
+//          let tr = Array.from(document.querySelectorAll("tbody > tr"));
+//          let trCount = 0;
+//          for (const line of tr) {
+//             let td = Array.from(line.querySelectorAll('td'));
+//             tr[trCount].classList.remove('select');
+//             trCount++;
+//          }
+//          for (const line of tr) {
+//             let found = false;
+//             let td = Array.from(line.querySelectorAll('td'));
+//             for (const col of td) {
+//                let word = col.innerText;
+//                if (word.includes(input)) {
+//                   tr[counter].classList.add('select');
+//                }
+//             }
+//             counter++;
+//          }
+//          document.querySelector("#searchField").value = '';
+//       }
+//    }
+// }
+
 function solve() {
-   //TODO...
-}
+   const rows = document.querySelectorAll('tbody > tr');
+   const input = document.getElementById('searchField');
+   const btn = document.getElementById('searchBtn');
+   const handler = () => {
+     Array.from(rows).map(e => (e.classList = ''));
+     Array.from(rows).forEach(e => {
+       Array.from(e.children).forEach(elem => {
+         const text = elem.textContent;
+ 
+         if (text.includes(input.value) && input.value !== '') {
+           elem.parentElement.className = 'select';
+         }
+       });
+     });
+     input.value = '';
+   };
+ 
+   btn.addEventListener('click', handler);
+ }
