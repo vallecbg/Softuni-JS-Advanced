249a12cf58959f727077798ed74567f1898f838f
vallecbg <valioplayofficial@gmail.com>
2019-11-20 20:51:41 +0200

    Added solution

--- JS Applications/Exercise Templating/02. HTTP Status Cats/template.js
+++ JS Applications/Exercise Templating/02. HTTP Status Cats/template.js
@@ -1,8 +1,27 @@
-(() => {
-     renderCatTemplate();
+function toggle(e) {
+    e.textContent = e.textContent === "Show status code"
+        ? "Hide status code"
+        : "Show status code";
+    const statusDiv = e.parentNode.getElementsByClassName("status")[0];
+    statusDiv.style.display = statusDiv.style.display === "none" ? "inline" : "none";
+}
 
-     function renderCatTemplate() {
-         // TODO: Render cat template and attach events
-     }
- 
-})
+(() => {
+    renderCatTemplate();
+
+    async function renderCatTemplate() {
+        const source = await fetch("http://127.0.0.1:5500/02.%20HTTP%20Status%20Cats/templates/all-cats.hbs")
+            .then(res => res.text());
+
+        const template = Handlebars.compile(source);
+        const context = {
+            cats: window.cats
+        };
+        const catsHtml = template(context);
+
+        document.getElementById("allCats")
+            .innerHTML = catsHtml;
+
+    }
+
+})()
--- JS Applications/Exercise Templating/03. Popular Monkeys/monkeysTemplate.js
+++ JS Applications/Exercise Templating/03. Popular Monkeys/monkeysTemplate.js
@@ -1,3 +1,25 @@
-$(() => {
-    // TODO
-})
+function showInfo(e) {
+    const infoDiv = e.parentNode.getElementsByClassName("info")[0];
+    infoDiv.style.display = infoDiv.style.display === "none" ? "block" : "none";
+}
+
+(() => {
+    renderMonkeyTemplate();
+
+    async function renderMonkeyTemplate() {
+        const source = await fetch("http://127.0.0.1:5500/03.%20Popular%20Monkeys/templates/all-monkeys.hbs")
+            .then(res => res.text());
+
+        const template = Handlebars.compile(source);
+
+        const context = {
+            monkeys: window.monkeys
+        };
+        const monkeysHtml = template(context);
+
+        document.getElementById("allMonkeys")
+            .innerHTML = monkeysHtml;
+
+    }
+
+})()
